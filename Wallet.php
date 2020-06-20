382529bfcb7bb8847a2957a2aa96012a7796a22a
dexterouz <dexterouskc@gmail.com>
2020-06-20 11:52:42 +0100

    finish implementing the function for sending coin

--- Wallet.php
+++ Wallet.php
@@ -125,16 +125,16 @@
         if ($stmt->affected_rows == 1) {
             $date = date('m:d:Y h:i:sa');
             $errorstring = $date." | transaction report sent |";
-            error_log($errorstring, 3, "logs/error_log.log");
+            // error_log($errorstring, 3, "logs/error_log.log");
             } else {
                 $date = date('m:d:Y h:i:sa');
                 $errorstring = $date." | Error in sending transaction report |";
-                error_log($errorstring, 3, "logs/error_log.log");
+                // error_log($errorstring, 3, "./logs/error_log.log");
             }
         } else {
             $date = date('m:d:Y h:i:sa');
             $errorstring = $date." | Error in sending transaction report |";
-            error_log($errorstring, 3, "logs/error_log.log");
+            // error_log($errorstring, 3, "logs/error_log.log");
         }
 
         // close connection to database
@@ -142,9 +142,9 @@
     }
 
     // method 'receiver_addr()'
-    function receiver_addr($recvr_addr)
+    function receiver_rec($recvr_addr)
     {
-        $recvr_addr_sql = "SELECT wallet_addr FROM wallet WHERE wallet_addr=?";
+        $recvr_addr_sql = "SELECT * FROM wallet WHERE wallet_addr=?";
         // prepare the statment
         $stmt = $this->connect->prepare($recvr_addr_sql);
         if (isset($stmt)) {
@@ -201,9 +201,9 @@
             // get the result of the statement execution
             $result = $stmt->get_result();
             if ($stmt->affected_rows == 1) {
-                $success[] = "Your purchase of {$get_purch_amount} coin is successful";
+                $success[] = "Your purchase of {$get_purch_amount} DXcoin was successful";
 
-                $report_message = "You purchased {$get_purch_amount} coin on ".date('m:d:Y h:i:sa');
+                $report_message = "You purchased {$get_purch_amount} DXcoin on ".date('m:d:Y h:i:sa').", your new balance is ".$wallet_rec['wallet_balance']." DXcoin";
                 // send the report of the transaction to the transaction report table
                 $this->sendTransactionReport($report_message, $wallet_rec['wallet_id'], $user_id);
             } else {
@@ -216,6 +216,113 @@
         // close database connection
         $stmt->close();
     } // End of if(empty($errors))
+
+        // store messages in array and return the array
+        $messages = array('errors' => $errors, 'success' => $success);
+        return $messages;
+    }
+
+    // method 'sendCoin()'
+    public function sendCoin($sendr_amount, $receiver_addr, $sendr_user_id)
+    {
+        // get sender wallet balance
+        $get_sendr_wal_rec = $this->walletRec($sendr_user_id);
+        // get sender wallet id
+        $get_sendr_wal_id = $get_sendr_wal_rec['wallet_id'];
+        // get sender wallet balance
+        $get_sendr_wal_bal = $get_sendr_wal_rec['wallet_balance'];
+        // get sender user name
+        $get_sendr_wal_name = $get_sendr_wal_rec['wallet_name'];
+
+        // get receiver's wallet record
+        $get_recvr_wal_rec = $this->receiver_rec($receiver_addr);
+
+        // get receiver user id
+        $get_recvr_wal_usrid = $get_recvr_wal_rec['user_id'];
+        // get receiver wallet id
+        $get_recvr_wal_id = $get_recvr_wal_rec['wallet_id'];
+        // get receiver wallet balance
+        $get_recvr_wal_bal = $get_recvr_wal_rec['wallet_balance'];
+        // get receiver wallet address
+        $get_recvr_addr = $get_recvr_wal_rec['wallet_addr'];
+        // get receiver's user name
+        $get_recvr_uname = $get_recvr_wal_rec['wallet_name'];
+
+        // array variable to store error message(s)
+        $errors = array();
+        // array variable to store success message(s)
+        $success = array();
+        
+        // validate sender amount input
+        $get_sendr_amount = htmlspecialchars($sendr_amount, ENT_QUOTES);
+        if (empty($get_sendr_amount)) {
+            $errors[] = "Enter amount";
+        }
+
+        // validate the receiver's address
+        // $get_recvr_addr = htmlspecialchars($receiver_addr, ENT_QUOTES);
+        // if (empty($get_recvr_addr)) {
+        //     $errors[] = "Enter receiver wallet address";
+        // }
+
+        if (empty($errors)) {
+            // check if the sending amount is greater than
+            // the sender's wallet account balance
+            if (($get_sendr_wal_bal) <= ($get_sendr_amount)) {
+                $errors[] = "You have an insufficient balance to make this transaction";
+            } else {
+                // proceed to update the receiver's wallet account balance
+                $recvr_wal_bal_sql = "UPDATE wallet SET wallet_balance = ".(($get_recvr_wal_bal) + ($get_sendr_amount))." ";
+                $recvr_wal_bal_sql .= "WHERE wallet_addr=?";
+                // prepare the statement
+                $stmt = $this->connect->prepare($recvr_wal_bal_sql);
+                // if the statement has been set
+                if (isset($stmt)) {
+                    // bind parameter to identifier
+                    $stmt->bind_param('s', $get_recvr_addr);
+                    // execute the statement
+                    $stmt->execute();
+
+                    // if the update is successful
+                    // print success message; else print error message
+                    if ($stmt->affected_rows) {
+                        // display transfer success message
+                        $success[] = "Transfer of {$get_sendr_amount} DXcoin to {$get_recvr_uname} was successful";
+
+                        $report_message = "You received {$get_sendr_amount} DXcoin from {$get_sendr_wal_name} on ".date('m:d:Y h:i:sa').", your new balance is ".($get_sendr_wal_bal)." DXcoin";
+                        // send the report of the transaction to the transaction report table
+                        $this->sendTransactionReport($report_message, $get_recvr_wal_id, $get_recvr_wal_usrid);
+
+                        // proceed to update the sender's wallet account balance
+                        $sendr_wal_bal_sql = "UPDATE wallet SET wallet_balance = ".(($get_sendr_wal_bal) - ($get_sendr_amount))." ";
+                        $sendr_wal_bal_sql .= "WHERE user_id=?";
+                        // prepare the statement
+                        $stmt = $this->connect->prepare($sendr_wal_bal_sql);
+                        // check if statement is set
+                        if (isset($stmt)) {
+                            // bind parameter to identifier
+                            $stmt->bind_param('i', $sendr_user_id);
+                            // execute the statement
+                            $stmt->execute();
+                            // check if row has been affected
+                            if ($stmt->affected_rows) {
+                                $report_message = "You transfered {$get_sendr_amount} DXcoin to {$get_recvr_uname} on ".date('m:d:Y h:i:sa').", your new balance is ".($get_sendr_wal_bal)." DXcoin";
+                                // send the report of the transaction to the transaction report table
+                                $this->sendTransactionReport($report_message, $get_sendr_wal_id, $sendr_user_id);
+                            }
+                        } else {
+                            $errors[] = "Transfer failed!";
+                        }
+                    } else {
+                        $errors[] = "Transfer failed!, receiver address is invalid";
+                    }
+                } else {
+                    $errors[] = "Transfer failed!";
+                }
+                // close connection to database
+                $stmt->close();
+            }
+        } # end of if(empty($errors))
 
         // store messages in array and return the array
         $messages = array('errors' => $errors, 'success' => $success);
